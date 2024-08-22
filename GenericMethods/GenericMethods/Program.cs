575525918ad447edc3e87313670262dc0dbc4f74
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-22 13:51:59 +0300

    Created extended generic method

---
+++ GenericMethods/GenericMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿var ints = new List<int> { 1, 2, 3 };
+ints.AddToFront(10);
+ints.AddToFront<int>(11);
+
+Console.ReadKey();
+
+static class ListExtensions
+{
+    public static void AddToFront<T>(this List<T> list, T item)
+    {
+        list.Insert(0, item);
+    }
+}
