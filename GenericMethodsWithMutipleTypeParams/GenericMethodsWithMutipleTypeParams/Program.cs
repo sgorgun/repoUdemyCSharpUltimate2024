56f566d9f3aba976a82d490661f83c9f30c7d421
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-22 14:14:53 +0300

    Added none generic method for converting decimal list to ineger list

---
+++ GenericMethodsWithMutipleTypeParams/GenericMethodsWithMutipleTypeParams/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+var decimals = new List<decimal> { 1.1m, 0.5m, 22.5m, 12m };
+var ints = decimals.ConvertTo();
+
+Console.ReadKey();
+
+static class ListExtensions
+{
+    public static void AddToFront<T>(this List<T> list, T item)
+    {
+        list.Insert(0, item);
+    }
+
+    public static List<int> ConvertTo(this List<decimal> decimals)
+    {
+        var result = new List<int>();
+
+        foreach (var item in decimals)
+        {
+            result.Add((int)item);
+        }
+
+        return result;
+    }
+}
