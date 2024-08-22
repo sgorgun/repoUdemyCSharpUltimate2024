6cd71be3c200bdb3c7fa0128d2f946ac6f9801b5
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-22 17:43:17 +0300

    Added timer. Used parameterless constractor.

--- TypeConstraints/TypeConstraints/Program.cs
+++ TypeConstraints/TypeConstraints/Program.cs
@@ -1,10 +1,15 @@
-﻿var ints = CreateCollectionsOfRandomLength<long>(100);
+﻿using System.Diagnostics;
+
+Stopwatch sw = Stopwatch.StartNew();
+var ints = CreateCollectionsOfRandomLength<long>(0);
+sw.Stop();
+Console.WriteLine($"Execution took {sw.ElapsedMilliseconds} ms.");
 
 Console.ReadKey();
 
 IEnumerable<T> CreateCollectionsOfRandomLength<T>(int maxLength) where T : new()
 {
-    var length = new Random().Next(maxLength + 1);
+    var length = 100_000_000; //new Random().Next(maxLength + 1);
     var result = new List<T>();
     
     for (int i = 0; i < length; ++i)
