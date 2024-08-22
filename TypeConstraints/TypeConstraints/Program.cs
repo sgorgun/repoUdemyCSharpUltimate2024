b270e4ef749a52b6a2de71cab6df4a700a76a0e0
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-22 17:19:32 +0300

    Implemented constrain

---
+++ TypeConstraints/TypeConstraints/Program.cs
@@ -0,0 +1,16 @@
+﻿var ints = CreateCollectionsOfRandomLength<long>(100);
+
+Console.ReadKey();
+
+IEnumerable<T> CreateCollectionsOfRandomLength<T>(int maxLength) where T : new()
+{
+    var length = new Random().Next(maxLength + 1);
+    var result = new List<T>();
+    
+    for (int i = 0; i < length; ++i)
+    {
+        result.Add(new T());
+    }
+
+    return result;
+}
