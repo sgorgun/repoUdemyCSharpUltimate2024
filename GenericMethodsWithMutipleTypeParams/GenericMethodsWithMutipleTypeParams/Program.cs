d0738152b3dd8269e31d0bacb7ff3c3e20cc549b
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-22 14:55:07 +0300

    Implemented

--- GenericMethodsWithMutipleTypeParams/GenericMethodsWithMutipleTypeParams/Program.cs
+++ GenericMethodsWithMutipleTypeParams/GenericMethodsWithMutipleTypeParams/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 var decimals = new List<decimal> { 1.1m, 0.5m, 22.5m, 12m };
-var ints = decimals.ConvertTo();
+var ints = decimals.ConvertTo<decimal, int>();
 
 Console.ReadKey();
 
@@ -12,13 +12,13 @@ static class ListExtensions
         list.Insert(0, item);
     }
 
-    public static List<int> ConvertTo(this List<decimal> decimals)
+    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> input)
     {
-        var result = new List<int>();
+        var result = new List<TTarget>();
 
-        foreach (var item in decimals)
+        foreach (var item in input)
         {
-            result.Add((int)item);
+            result.Add((TTarget)item);
         }
 
         return result;
---
+++ TupleSwap/TupleSwap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = new Tuple<int, string>(1, "hello");
+            var output = input.SwapTupleItems();
+            Console.WriteLine($"Swapped Tuple: Item1 = {output.Item1}, Item2 = {output.Item2}");
+            Console.ReadKey();
+        }
+    }
+
+    public static class TupleSwapExercise
+    {
+        public static Tuple<T2, T1> SwapTupleItems<T1, T2>(this Tuple<T1, T2> input) =>
+            new(input.Item2, input.Item1);
+    }
+}
