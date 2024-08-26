2d237230110df914e92900545c869441d48e776a
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-26 23:24:44 +0300

    Numeric types constraints. INumber Interface

---
+++ NumericTypesConstraints/NumericTypesConstraints/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Square of 2 is: " + Calculator.Square(2));
+Console.WriteLine("Square of 4m is: " + Calculator.Square(4m));
+Console.WriteLine("Square of 6d is: " + Calculator.Square(6d));
+
+Console.ReadLine();
+
+public static class Calculator
+{
+    public static T Square<T>(T input) where T : INumber<T>
+        => input * input;
+}
