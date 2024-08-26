b526a9c10c3c800118a425d87eaab5c2f412c392
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-26 17:27:44 +0300

    Custom sorting and IComparable implemented.

--- BaseClassConstrains/BaseClassConstrains/Program.cs
+++ BaseClassConstrains/BaseClassConstrains/Program.cs
@@ -1,11 +1,18 @@
-﻿var people = new List<Person>
+﻿var numbers = new List<int> { 5, 1, 7, 2 };
+numbers.Sort();
+
+var words = new List<string> { "ddd", "aaa", "ccc", "bbb" };
+words.Sort();
+
+var people = new List<Person>
 {
     new Person {Name = "John", YearOfBirth = 1980},
     new Person {Name = "Anna", YearOfBirth = 1880},
     new Person {Name = "Bill", YearOfBirth = 2980},
 };
+people.Sort();
 
-var employees = new List<Employee>
+ var employees = new List<Employee>
 {
     new Employee {Name = "John", YearOfBirth = 1980},
     new Employee {Name = "Anna", YearOfBirth = 1880},
@@ -38,10 +45,24 @@ IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> persons) where T
     return results;
 }
 
-public class Person
+public class Person : IComparable<Person>
 {
     public string Name { get; init; }
     public int YearOfBirth { get; init; }
+
+    public int CompareTo(Person? other)
+    {
+        if (this.YearOfBirth < other.YearOfBirth)
+        {
+            return 1;
+        }
+        else if (this.YearOfBirth > other.YearOfBirth)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
 }
 
 public class Employee : Person
