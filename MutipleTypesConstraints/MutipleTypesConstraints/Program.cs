1064a6f9303debbd9e7c1b3af5bdcb3a734a58e9
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-26 23:36:12 +0300

    Multipe types constraints

---
+++ MutipleTypesConstraints/MutipleTypesConstraints/Program.cs
@@ -0,0 +1,10 @@
+﻿void SomeMethod<TPet, TOwner>(TPet pet, TOwner owner)
+    where TPet : Pet, IComparable<TPet>
+    where TOwner : new()
+{
+    throw new NotImplementedException();
+}
+
+
+public class Pet { }
+public class PetOwner { }
