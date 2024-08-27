73741a67eefeae7fb77e72e9a7db055197edee0f
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-27 17:47:44 +0300

    Predicate is converted to lambda expressions

--- FuncsAndActions/FuncsAndActions/Program.cs
+++ FuncsAndActions/FuncsAndActions/Program.cs
@@ -5,10 +5,8 @@ var john = new Person("John", "Smith", 1981);
 
 var numbers = new[] { 1, 4, 7, 19, 2 };
 
-Func<int, bool> predicate = IsAnyLagerThan10;
-
-Console.WriteLine("IsAnyLagerThan10?" + IsAny(numbers, predicate));
-Console.WriteLine("IsAnyEven?" + IsAny(numbers, IsAnyEven));
+Console.WriteLine("IsAnyLagerThan10?" + IsAny(numbers, n => n > 10));
+Console.WriteLine("IsAnyEven?" + IsAny(numbers, n => n % 2 == 0));
 
 Func<int, DateTime, string, decimal> someFunc; // all parameters are input instead of last. Last parameter is output.
 Action<string, string, bool> someAction; // all parameters are input. Action use for void action without output.
@@ -24,14 +22,6 @@ bool IsAny(IEnumerable<int> numbers, Func<int, bool> predicate)
     return false;
 }
 
-bool IsAnyLagerThan10(int number) //method1
-{
-    return number > 10;
-}
-bool IsAnyEven(int number) // method2
-{
-    return number % 2 == 0;
-}
 
 public class Person(string firstName, string secondName, int yearOfBirth)
 {
