39b17ce18cdfdd61ea9e0f21c8c93fa898fd275e
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-31 15:11:42 +0300

    Simple delegate

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿ProcessString processString1 = TrimTo5Letters;
+ProcessString processString2 = ToUpper;
+Console.WriteLine(processString1("Hellooooooooooooooo"));
+Console.WriteLine(processString2("Hellooooooooooooooo"));
+Console.ReadKey();
+string TrimTo5Letters(string input)
+{
+    return input.Substring(0, 5);
+}
+
+string ToUpper(string input)
+{
+    return input.ToUpper();
+}
+
+delegate string ProcessString(string input);
