bb648105a1bc36db909959696d5b0c1aac641f20
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-08-31 16:54:01 +0300

    Dictionary implemented

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿var countryToCurrencyMapping = new Dictionary<string, string>
+{
+    ["USA"] = "USD",
+    ["India"] = "INR",
+    ["Spain"] = "EUR",
+
+};
+
+//countryToCurrencyMapping.Add("USA", "USD");
+//countryToCurrencyMapping.Add("India", "INR");
+//countryToCurrencyMapping.Add("Spain", "EUR");
+
+//Console.WriteLine("Currency in Spain is " +
+//    countryToCurrencyMapping["Spain"]);
+
+countryToCurrencyMapping["Poland"] = "PLN";
+countryToCurrencyMapping["Poland"] = "EUR";
+
+//Console.WriteLine("Currency in Poland is " +
+//    countryToCurrencyMapping["Poland"]);
+
+//if (countryToCurrencyMapping.ContainsKey("Italy"))
+//{
+//    Console.WriteLine("Currency in Italy is " +
+//    countryToCurrencyMapping["Italy"]);
+//}
+
+foreach (var item in countryToCurrencyMapping)
+{
+    Console.WriteLine($"Currency in {item.Key} is " + $"{item.Value}");
+}
+
+Console.ReadKey();
