e32d50ca8b35daf46a5463b224e076d8b42e5692
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-01 21:12:07 +0300

    refactored whitout func

--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -6,72 +6,76 @@ Odd
 Positive");
 
 var userInput = Console.ReadLine();
-List<int> result;
-switch (userInput)
-{
-    case "Even":
-        result = SelectEven(numbers);
-        break;
-    case "Odd":
-        result = SelectOdd(numbers);
-        break;
-    case "Positive":
-        result = SelectPositive(numbers);
-        break;
-    default:
-        throw new NotSupportedException($"{userInput} is not a valid filter");
-}
+List<int> result = new NumbersFilter().FilterBy(userInput, numbers);
 
 Print(result);
 
 Console.ReadKey();
 
-List<int> SelectEven(List<int> numbers)
-{
-    var result = new List<int>();
-
-    foreach (var number in numbers)
-    {
-        if (number % 2 == 0)
-        {
-            result.Add(number);
-        }
-    }
-
-    return result;
-}
-
-List<int> SelectOdd(List<int> numbers)
-{
-    var result = new List<int>();
-
-    foreach (var number in numbers)
-    {
-        if (number % 2 == 1)
-        {
-            result.Add(number);
-        }
-    }
-
-    return result;
-}
-
-List<int> SelectPositive(List<int> numbers)
-{
-    var result = new List<int>();
-
-    foreach (var number in numbers)
-    {
-        if (number > 0)
-        {
-            result.Add(number);
-        }
-    }
-
-    return result;
-}
-
 void Print(IEnumerable<int> numbers)
 {
     Console.WriteLine(string.Join(", ", numbers));
+}
+
+public class NumbersFilter
+{
+    public List<int> FilterBy(string filteringType, List<int> numbers)
+    {
+        switch (filteringType)
+        {
+            case "Even":
+                return SelectEven(numbers);
+            case "Odd":
+                return SelectOdd(numbers);
+            case "Positive":
+                return SelectPositive(numbers);
+            default:
+                throw new NotSupportedException($"{filteringType} is not a valid filter");
+        }
+    }
+
+    private List<int> SelectEven(List<int> numbers)
+    {
+        var result = new List<int>();
+
+        foreach (var number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                result.Add(number);
+            }
+        }
+
+        return result;
+    }
+
+    private List<int> SelectOdd(List<int> numbers)
+    {
+        var result = new List<int>();
+
+        foreach (var number in numbers)
+        {
+            if (number % 2 == 1)
+            {
+                result.Add(number);
+            }
+        }
+
+        return result;
+    }
+
+    private List<int> SelectPositive(List<int> numbers)
+    {
+        var result = new List<int>();
+
+        foreach (var number in numbers)
+        {
+            if (number > 0)
+            {
+                result.Add(number);
+            }
+        }
+
+        return result;
+    }
 }
