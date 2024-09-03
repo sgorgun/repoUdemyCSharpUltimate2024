1d189d686a02f30e49a5998eb43fa8703b0c4ddb
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-03 16:42:14 +0300

    Multiple Decorator implemented

--- Caching/Caching/Program.cs
+++ Caching/Caching/Program.cs
@@ -1,5 +1,8 @@
 ﻿//var dataDownloader = new SlowDataDownloader();// Don't using caching
-IDataDownloader dataDownloader = new CachigDataDownloader(new SlowDataDownloader());// Using caching
+IDataDownloader dataDownloader =
+    new CachigDataDownloader(
+        new PrintingDataDownloader(
+            new SlowDataDownloader()));// Using caching
 
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -11,7 +14,7 @@ Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
-public class Cache<TKey,TData>
+public class Cache<TKey, TData>
 {
     private readonly Dictionary<TKey, TData> _cacheData = new();
 
@@ -42,13 +45,30 @@ public class CachigDataDownloader : IDataDownloader
     }
 }
 
+public class PrintingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+
+    public PrintingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        var data = _dataDownloader.DownloadData(resourceId);
+        Console.WriteLine("Data is ready!");
+        return data;
+    }
+}
+
 public interface IDataDownloader
 {
     string DownloadData(string resourceId);
 }
 
 public class SlowDataDownloader : IDataDownloader
-{ 
+{
     public string DownloadData(string resourceId)
     {
         Thread.Sleep(1000);
