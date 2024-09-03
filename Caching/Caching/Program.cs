5c4456017c98bee7c6788b604c78a67bf693648e
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-03 14:51:06 +0300

    The simple cache implemented.

--- Caching/Caching/Program.cs
+++ Caching/Caching/Program.cs
@@ -10,6 +10,22 @@ Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
+public class Cache<TKey,TData>
+{
+    private readonly Dictionary<TKey, TData> _cacheData = new();
+
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+    {
+        if (!_cacheData.ContainsKey(key))
+        {
+            _cacheData[key] = getForTheFirstTime(key);
+        }
+
+        return _cacheData[key];
+    }
+
+}
+
 public interface IDataDownloader
 {
     string DownloadData(string resourceId);
@@ -17,7 +33,13 @@ public interface IDataDownloader
 
 public class SlowDataDownloader : IDataDownloader
 {
+    private readonly Cache<string, string> _cache = new();
     public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, DownloadDataWithoutCaching);
+    }
+    
+    private string DownloadDataWithoutCaching(string resourceId)
     {
         Thread.Sleep(1000);
         return $"Some data for {resourceId}";
