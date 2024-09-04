d4bce547a6be285c8d3a005163b76b2c27fe5c95
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-04 17:42:12 +0300

    Linq Methods implemented

--- LinqBasic/LinqBasic/Program.cs
+++ LinqBasic/LinqBasic/Program.cs
@@ -11,14 +11,22 @@ class Program
             "quick", "brown", "fox"
         };
         Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));
+        Console.WriteLine(IsAnyWordUpperCaseLinq(wordsNoUpperCase));
+
         
         var wordsUpperCase = new string[]
         {
             "quick", "brown", "FOX"
         };
         Console.WriteLine(IsAnyWordUpperCase(wordsUpperCase));
+        Console.WriteLine(IsAnyWordUpperCaseLinq(wordsUpperCase));
 
         Console.ReadKey();
+    }
+
+    public static bool IsAnyWordUpperCaseLinq(IEnumerable<string> words)
+    {
+        return words.Any(word => word.All(letter => char.IsUpper(letter)));
     }
 
     public static bool IsAnyWordUpperCase(IEnumerable<string> words)
