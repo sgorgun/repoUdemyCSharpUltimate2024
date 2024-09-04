4cb177030131db9fc1d3c09c0cecce5a8b8f569f
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-04 21:17:57 +0300

    Some linq methods were inplemented

---
+++ LinqWithDifferentMethods/LinqWithDifferentMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿var words = new List<string> { "a", "bb", "ccc", "dddd" };
+var wordsLongerThan2Letters = words.Where(letter => letter.Count() > 2);
+foreach (var word in wordsLongerThan2Letters)
+{
+    Console.WriteLine(word + " ");
+}
+
+var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+var oddNumbers = numbers.Where(number => number % 2 == 1);
+foreach (var number in oddNumbers)
+{
+    Console.WriteLine(number + " ");
+}
+
+Console.ReadKey();
