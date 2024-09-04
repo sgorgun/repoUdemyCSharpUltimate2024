a7ad7083f557fa0871e0c5eab374a37d7b8fa072
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-04 15:50:52 +0300

    Upper and Lower case without LINQ

---
+++ LinqBasic/LinqBasic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var wordsNoUpperCase = new string[]
+        {
+            "quick", "brown", "fox"
+        };
+        Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));
+        
+        var wordsUpperCase = new string[]
+        {
+            "quick", "brown", "FOX"
+        };
+        Console.WriteLine(IsAnyWordUpperCase(wordsUpperCase));
+
+        Console.ReadKey();
+    }
+
+    public static bool IsAnyWordUpperCase(IEnumerable<string> words)
+    {
+        foreach (string word in words)
+        {
+            bool areAllUpperCase = true;
+            foreach (var letter in word)
+            {
+                if (char.IsLower(letter))
+                {
+                    areAllUpperCase = false;
+                }
+            }
+
+            if (areAllUpperCase)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
