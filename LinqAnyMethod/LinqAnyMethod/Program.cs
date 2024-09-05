27e765e1d3580ea93968aa79f3fb0f4ad5b07e57
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-05 20:28:41 +0300

    Linq Count Method implemented

--- LinqAnyMethod/LinqAnyMethod/Program.cs
+++ LinqAnyMethod/LinqAnyMethod/Program.cs
@@ -6,9 +6,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        var numbers = new List<int> { 5, 9, 2, 12, 6 };
-        bool areAllLargerThanZero = numbers.Any(number => number > 10);
-        Printer.Print(areAllLargerThanZero, nameof(areAllLargerThanZero));
+        //var numbers = new List<int> { 5, 9, 2, 12, 6 };
+        //bool areAllLargerThanZero = numbers.Any(number => number > 10);
+        //Printer.Print(areAllLargerThanZero, nameof(areAllLargerThanZero));
 
         var pets = new[]
         {
@@ -22,11 +22,23 @@ class Program
             new Pet(8, "Nyan", PetType.Cat, 2.2f)
         };
 
-        var doAllHaveNoneEmptyNames = pets.All(pet => !string.IsNullOrWhiteSpace(pet.Name));
-        Printer.Print(doAllHaveNoneEmptyNames, nameof(doAllHaveNoneEmptyNames));
+        var countOfDogs = pets.Count(pet => pet.Type == PetType.Dog);
+        Console.WriteLine(countOfDogs);
+
+        var countOfPetsNamedBruce = pets.Count(pet => pet.Name == "Bruce");
+        Console.WriteLine("Bruce: " + countOfPetsNamedBruce);
+
+        var allSmallDogs = pets.Count(pet => pet.Type == PetType.Dog && pet.Weight < 10);
+        Console.WriteLine("Small dogs: " + allSmallDogs);
+
+        var allPetsCount = pets.Count();
+        Console.WriteLine("All pets: " + allPetsCount);
+
+        //var doAllHaveNoneEmptyNames = pets.All(pet => !string.IsNullOrWhiteSpace(pet.Name));
+        //Printer.Print(doAllHaveNoneEmptyNames, nameof(doAllHaveNoneEmptyNames));
         
-        var doAllPetsAreCats = pets.All(pet => pet.Type == PetType.Cat);
-        Printer.Print(doAllPetsAreCats, nameof(doAllPetsAreCats));
+        //var doAllPetsAreCats = pets.All(pet => pet.Type == PetType.Cat);
+        //Printer.Print(doAllPetsAreCats, nameof(doAllPetsAreCats));
 
         //var isAnyPetNamedBruce = pets.Any(pet => pet.Name == "Bruce");
         //Printer.Print(isAnyPetNamedBruce, nameof(isAnyPetNamedBruce));
