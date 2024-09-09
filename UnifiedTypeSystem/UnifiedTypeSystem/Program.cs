800d447f46871906ed112c046c806e80a42cafd1
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-09 23:25:52 +0300

    List of objects implemented

---
+++ UnifiedTypeSystem/UnifiedTypeSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿var variousObjects = new List<object>
+{
+    1,
+    1.5m,
+    new DateTime(2024, 6, 30),
+    "hello",
+    new Person {Name = "Anna", Age = 61}
+};
+
+foreach (var variousObject in variousObjects)
+{
+    Console.WriteLine(variousObject.GetType().Name);
+}
+
+Console.ReadKey();
+
+class Person
+{
+    public string Name { get; init; }
+    public int Age { get; init; }
+}
