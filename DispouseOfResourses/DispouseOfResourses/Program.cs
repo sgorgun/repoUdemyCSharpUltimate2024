a04aadad3ef95d8d759487d0a6e8b877adab1cb4
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-12 23:40:31 +0300

    Steam reader is implemented without the dispose method

--- DispouseOfResourses/DispouseOfResourses/Program.cs
+++ DispouseOfResourses/DispouseOfResourses/Program.cs
@@ -3,6 +3,13 @@ var writer = new FileWriter(filePath);
 writer.Write("some text");
 writer.Write("some other text");
 
+var reader = new SpecificLineFromTextReader(filePath);
+var third = reader.ReadLineNumber(3);
+var fourth = reader.ReadLineNumber(4);
+
+Console.WriteLine("Third line is: " + third);
+Console.WriteLine("Fourth line is: " + fourth);
+
 Console.WriteLine("Press any key for close.");
 Console.ReadKey();
 
@@ -17,5 +24,27 @@ public class FileWriter
     {
         _streamWriter.WriteLine(text);
         _streamWriter.Flush();
+    }
+}
+
+public class SpecificLineFromTextReader
+{
+    private readonly StreamReader _streamReader;
+    public SpecificLineFromTextReader(string filePath)
+    {
+        _streamReader = new StreamReader(filePath);
+    }
+
+    public string ReadLineNumber(int lineNumber)
+    {
+        _streamReader.DiscardBufferedData(); //clear buffer
+        _streamReader.BaseStream.Seek(0, SeekOrigin.Begin);// move reader in the begining
+        
+        for (int i = 0; i < lineNumber - 1; i++)
+        {
+            _streamReader.ReadLine();
+        }
+
+        return _streamReader.ReadLine();
     }
 }
