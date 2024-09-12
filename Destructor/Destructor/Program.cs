4eab8c1e746bccab97c101e72405ecfd224a709d
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-12 22:32:26 +0300

    Distractor is implemented

---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,20 @@
+﻿for (int i = 0; i < 5; i++)
+{
+    var person = new Person { Name = "Shivay", Age = 37 };
+}
+
+GC.Collect();
+Console.WriteLine("Ready to close.");
+
+Console.ReadKey();
+
+class Person
+{
+    public string Name { get; init; }
+    public int Age { get; init; }
+
+    ~Person()
+    {
+        Console.WriteLine($"Person called {Name} is being destructed.");
+    }
+}
