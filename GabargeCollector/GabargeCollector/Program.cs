0aa4c2a376087bbff20f25243b61c2d530db807b
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-12 16:57:29 +0300

    GC implemented

---
+++ GabargeCollector/GabargeCollector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+string userInput = Console.ReadLine();
+if (userInput == "Print r=person")
+{
+    Person person = new Person("Shivay", 37);
+    Console.WriteLine($"{person.Name} is {person.Age} years old.");
+}
+
+GC.Collect();
+
+Console.ReadKey();
+
+public class Person(string name, int age)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+}
