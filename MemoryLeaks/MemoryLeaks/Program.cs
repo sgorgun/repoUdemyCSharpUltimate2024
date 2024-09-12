886dcf1e74a136bf606066178c9fccbc929aff42
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-12 21:43:56 +0300

    static list implemented

---
+++ MemoryLeaks/MemoryLeaks/Program.cs
@@ -0,0 +1,22 @@
+﻿bool someCondition = true;
+
+if (someCondition)
+{
+    var someClassInstance = new SomeClass(); 
+}
+
+Console.WriteLine("Count of all instance is now " + SomeClass.CountOfInstances);
+
+Console.ReadKey();
+
+public class SomeClass
+{
+    private static List<SomeClass> _allExistingInstances = new List<SomeClass>();
+
+    public static int CountOfInstances => _allExistingInstances.Count;
+
+    public SomeClass()
+    {
+        _allExistingInstances.Add(this);
+    }
+}
