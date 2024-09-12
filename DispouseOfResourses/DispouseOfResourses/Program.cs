69d8197dff59f3c7a7ae57f7f53d4c15fb0a3661
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-12 23:03:39 +0300

    Stream writer is implemented

---
+++ DispouseOfResourses/DispouseOfResourses/Program.cs
@@ -0,0 +1,21 @@
+﻿const string filePath = "file.txt";
+var writer = new FileWriter(filePath);
+writer.Write("some text");
+writer.Write("some other text");
+
+Console.WriteLine("Press any key for close.");
+Console.ReadKey();
+
+public class FileWriter
+{
+    public readonly StreamWriter _streamWriter;
+    public FileWriter(string filePath)
+    {
+        _streamWriter = new StreamWriter(filePath, true);
+    }
+    public void Write(string text)
+    {
+        _streamWriter.WriteLine(text);
+        _streamWriter.Flush();
+    }
+}
