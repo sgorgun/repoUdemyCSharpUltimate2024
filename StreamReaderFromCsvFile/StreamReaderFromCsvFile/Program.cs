37131f5a00ad13b498598d152af04b05b5e60a0c
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-16 16:05:32 +0300

    CSV reader is impemented

---
+++ StreamReaderFromCsvFile/StreamReaderFromCsvFile/Program.cs
@@ -0,0 +1,38 @@
+﻿const string path = @"C:\Users\serge\Downloads\sampleData.csv";
+
+var data = new CsvReader().Read(path);
+
+Console.ReadKey();
+
+public class CsvReader
+{
+    public CsvData Read(string path)
+    {
+        using var streamReader = new StreamReader(path);
+
+        const string Separator = ",";
+        var columns = streamReader.ReadLine().Split(Separator);
+
+        var rows = new List<string[]>();
+
+        while (!streamReader.EndOfStream)
+        {
+            var cellsInRow = streamReader.ReadLine().Split(Separator);
+            rows.Add(cellsInRow);
+        }
+
+        return new CsvData(columns, rows);
+    }
+}
+
+public class CsvData
+{
+    public string[] Columns { get; }
+    public IEnumerable<string[]> Rows { get; }
+
+    public CsvData(string[] columns, IEnumerable<string[]> rows)
+    {
+        Columns = columns;
+        Rows = rows;
+    }
+}
