433a35c58a8b55c87c4d5316d54105d6f01e3046
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-17 13:49:31 +0300

    Reflection is implemented

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+var converter = new ObjectToTextConverter();
+Console.WriteLine(converter.Convert(new House("123 Maple Road", 170.6, 2)));
+
+class ObjectToTextConverter
+{
+    public string Convert(object obj)
+    {
+        Type type = obj.GetType();
+        var properties = type
+            .GetProperties()
+            .Where(p => p.Name != "EqualityContract");
+
+        return String.Join(", ", properties
+            .Select(p => $"{p.Name} is {p.GetValue(obj)}"));
+    }
+}
+
+public record Pet(string Name, PetType PetType, float Weight);
+public record House(string Address, double Area, int Floors);
+public enum PetType { Cat, Dog, Fish };
