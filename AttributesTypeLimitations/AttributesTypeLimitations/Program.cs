b17c2406017a9da19ec32eeb1217f8bc85869c6b
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-18 23:38:40 +0300

    Atributs types limitation implemented

---
+++ AttributesTypeLimitations/AttributesTypeLimitations/Program.cs
@@ -0,0 +1,15 @@
+﻿[Some(new int[] { 1, 2, 3 })]
+public class SoneClass
+{
+
+}
+
+public class SomeAttribute : Attribute
+{
+    public SomeAttribute(int[] numbers)
+    {
+        Numbers = numbers;
+    }
+
+    public int[] Numbers { get; }
+}
