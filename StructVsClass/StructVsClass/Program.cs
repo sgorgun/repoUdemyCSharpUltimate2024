764875cdea8e4310bc4c321c5d660484894446d2
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-19 00:25:51 +0300

    Struct and class constrain are implemented

---
+++ StructVsClass/StructVsClass/Program.cs
@@ -0,0 +1,35 @@
+﻿var point = new Point(1, 3);
+var anotherPoint = point;
+anotherPoint.Y = 100;
+
+Console.WriteLine("point is: " + point);
+Console.WriteLine("anotherPoint is: " + anotherPoint);
+
+SomeMethodVal(5);
+SomeMethodRef(new Person());
+
+Console.ReadKey();
+
+void SomeMethodVal<T>(T param) where T : struct
+{
+
+}
+
+void SomeMethodRef<T>(T param) where T : class
+{
+
+}
+
+struct Point(int x, int y)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
+
+class Person
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+}
