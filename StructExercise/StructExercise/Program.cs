9efd93575f375eab881d0cfc01bb9552aeba4c67
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-19 17:10:15 +0300

    Struct Exercise is implemented

---
+++ StructExercise/StructExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public struct Time
+    {
+        public int Hour { get; }
+
+        public int Minute { get; }
+
+        public Time(int hour, int minute)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hour), "Hour must be between 0 and 23.");
+            }
+
+            if (minute < 0 || minute > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minute), "Minute must be between 0 and 59.");
+            }
+
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public override string ToString() => $"{Hour.ToString("00")}:{Minute.ToString("00")}";
+    }
+}
