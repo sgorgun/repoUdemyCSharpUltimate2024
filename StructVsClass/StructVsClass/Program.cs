3ab67d57858e48a0426be6158984d62d3c36471d
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-19 16:10:17 +0300

    inmutable struct implemented

--- StructVsClass/StructVsClass/Program.cs
+++ StructVsClass/StructVsClass/Program.cs
@@ -1,4 +1,10 @@
-﻿var point = new Point(1, 3);
+﻿
+var point = new Point(10, 20);
+point.X++;
+MoveToRightBy1Unit(point);
+
+
+
 var anotherPoint = point;
 anotherPoint.Y = 100;
 
@@ -8,7 +14,24 @@ Console.WriteLine("anotherPoint is: " + anotherPoint);
 SomeMethodVal(5);
 SomeMethodRef(new Person());
 
+
+//Point nullPoint = null;
+Person nullPerson = null;
+
+var person = new Person();
+var point1 = new Point();
+
+var fishyStruct1 = new FishyStruct { Numbers = new List<int> { 1, 2, 3 } };
+var fishyStruct2 = fishyStruct1;
+fishyStruct2.Numbers.Clear();
+
+
 Console.ReadKey();
+
+void MoveToRightBy1Unit(Point point)
+{
+    point.X++;
+}
 
 void SomeMethodVal<T>(T param) where T : struct
 {
@@ -20,16 +43,34 @@ void SomeMethodRef<T>(T param) where T : class
 
 }
 
+struct FishyStruct
+{
+    public List<int> Numbers { get; init; }
+}
+
 struct Point(int x, int y)
 {
-    public int X { get; set; } = x;
-    public int Y { get; set; } = y;
+    public int X { get; init; } = x; //inmutable struct 
+    public int Y { get; init; } = y;
+
+    //~Point()
+    //{
+
+    //}
 
     public override string ToString() => $"X: {X}, Y: {Y}";
 }
 
 class Person
 {
+    private Point _favouritePoint;
+    private Person _favouritePerson;
     public int Id { get; init; }
     public string Name { get; init; }
+
+    //public Person(int id, string name)
+    //{
+    //    Id = id;
+    //    Name = name;
+    //}
 }
