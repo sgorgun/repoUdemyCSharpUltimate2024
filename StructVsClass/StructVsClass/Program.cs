1132d6a402ef3aa0eecbb87a6a905f0d6d5e372c
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-24 14:53:44 +0300

    Equals method is implemented

--- StructVsClass/StructVsClass/Program.cs
+++ StructVsClass/StructVsClass/Program.cs
@@ -1,47 +1,66 @@
 ﻿
 using System.Globalization;
 
-var point = new Point(10, 20);
-//point.X++;
-//MoveToRightBy1Unit(point);
-var pointMoved = point with { X = point.X + 1 };
+//var point = new Point(10, 20);
+////point.X++;
+////MoveToRightBy1Unit(point);
+//var pointMoved = point with { X = point.X + 1 };
 
-var pet = new { Name = "Hanibal", Type = "fish" }; // anonimus type whith nonedestructiwe mutation
-var asCrab = pet with { Type = "crab" };
+//var pet = new { Name = "Hanibal", Type = "fish" }; // anonimus type whith nonedestructiwe mutation
+//var asCrab = pet with { Type = "crab" };
 
-var anotherPoint = point;
-//anotherPoint.Y = 100;
+//var anotherPoint = point;
+////anotherPoint.Y = 100;
 
-Console.WriteLine("point is: " + point);
-Console.WriteLine("anotherPoint is: " + anotherPoint);
+//Console.WriteLine("point is: " + point);
+//Console.WriteLine("anotherPoint is: " + anotherPoint);
 
-SomeMethodVal(5);
-//SomeMethodRef(new Person());
+//SomeMethodVal(5);
+////SomeMethodRef(new Person());
 
 
-//Point nullPoint = null;
-Person nullPerson = null;
+////Point nullPoint = null;
+//Person nullPerson = null;
 
-//var person = new Person();
-var point1 = new Point();
+////var person = new Person();
+//var point1 = new Point();
 
-var fishyStruct1 = new FishyStruct { Numbers = new List<int> { 1, 2, 3 } };
-var fishyStruct2 = fishyStruct1;
-fishyStruct2.Numbers.Clear();
+//var fishyStruct1 = new FishyStruct { Numbers = new List<int> { 1, 2, 3 } };
+//var fishyStruct2 = fishyStruct1;
+//fishyStruct2.Numbers.Clear();
 
-var dateTime = new DateTime(2023, 6, 7);
-var dateWeekAfter = dateTime.AddDays(7);
+//var dateTime = new DateTime(2023, 6, 7);
+//var dateWeekAfter = dateTime.AddDays(7);
+
+//var john = new Person(1, "John");
+//var theSameAsJohn = new Person(1, "John");
+//var asJohn = john;
+//Console.WriteLine("Are reference equal? " +
+//    object.ReferenceEquals(john, theSameAsJohn));
+//Console.WriteLine("Are reference equal? " +
+//    object.ReferenceEquals(john, asJohn)); 
+
+//Console.WriteLine(object.ReferenceEquals(1, 1)); //Value
+//Console.WriteLine(object.ReferenceEquals(null, null));
+
+Console.WriteLine("1.Equals(1): " + 1.Equals(1));
+Console.WriteLine("1.Equals(2): " + 1.Equals(2));
+Console.WriteLine("1.Equals(null): " + 1.Equals(null));
+Console.WriteLine("\"abc\".Equals(\"abc\"): " + "abc".Equals("abc"));
+Console.WriteLine();
+
+var point1 = new Point(1, 5);
+var point2 = new Point(1, 5);
+Console.WriteLine("point1.Equals(point2): " + point1.Equals(point2));
+Console.WriteLine();
 
 var john = new Person(1, "John");
 var theSameAsJohn = new Person(1, "John");
-var asJohn = john;
-Console.WriteLine("Are reference equal? " +
-    object.ReferenceEquals(john, theSameAsJohn));
-Console.WriteLine("Are reference equal? " +
-    object.ReferenceEquals(john, asJohn)); 
+var marie = new Person(2, "Marie");
 
-Console.WriteLine(object.ReferenceEquals(1, 1)); //Value
-Console.WriteLine(object.ReferenceEquals(null, null));
+Console.WriteLine("john.Equals(theSameAsJohn): " + john.Equals(theSameAsJohn));
+Console.WriteLine("john.Equals(marie): " + john.Equals(marie));
+Console.WriteLine("john.Equals(null): " + john.Equals(null));
 
 Console.ReadKey();
 
