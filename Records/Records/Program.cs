ed5fe36b224074b2084b05a08f3274027a65ace9
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-29 22:22:23 +0300

    implemented without records

---
+++ Records/Records/Program.cs
@@ -0,0 +1,48 @@
+﻿
+var weatherData = new WeatherData(25.1m, 65);
+Console.WriteLine(weatherData);
+Console.ReadKey();
+
+public class WeatherData : IEquatable<WeatherData?>
+{
+    public decimal Temperature { get; }
+    public int Humidity { get; }
+
+    public WeatherData(decimal temperature, int humidity)
+    {
+        Temperature = temperature;
+        Humidity = humidity;
+    }
+
+    public override string ToString()
+    {
+        return $"Temperature: {Temperature}, Humidity: {Humidity}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as WeatherData);
+    }
+
+    public bool Equals(WeatherData? other)
+    {
+        return other is not null &&
+               Temperature == other.Temperature &&
+               Humidity == other.Humidity;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Temperature, Humidity);
+    }
+
+    public static bool operator ==(WeatherData? left, WeatherData? right)
+    {
+        return EqualityComparer<WeatherData>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(WeatherData? left, WeatherData? right)
+    {
+        return !(left == right);
+    }
+}
