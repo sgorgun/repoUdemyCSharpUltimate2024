dddc58088033c170f45585f69d9b731a6b70ae31
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-29 23:30:47 +0300

    Nullable value type is implemented

---
+++ NullableType/NullableType/Program.cs
@@ -0,0 +1,28 @@
+﻿string text = null;
+
+int? numberOrNull = null;
+Nullable<bool> boolOrNull = true;
+
+var heights = new List<Nullable<int>>
+{
+    160, null, 185, null, 170
+};
+
+var averagHeight = heights
+    .Average();
+
+Console.WriteLine("Averrage height is " + averagHeight);
+
+if(numberOrNull.HasValue)
+{
+    int number = numberOrNull.Value;
+    Console.WriteLine("Not null");
+}
+
+if (boolOrNull.HasValue)
+{
+    var someBool = boolOrNull.Value;
+    Console.WriteLine(someBool);
+}
+
+Console.ReadKey();
