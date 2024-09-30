ffa9d00afa6eb7a2f96728b2956970d22e7ca948
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-30 16:55:15 +0300

    API is queried

---
+++ ApiHttpClient/ApiHttpClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using var client = new HttpClient();
+client.BaseAddress = new Uri("https://datausa.io/api/");
+var response = await client.GetAsync("data?drilldowns=Nation&measures=Population");
+response.EnsureSuccessStatusCode();
+
+string json = await response.Content.ReadAsStringAsync();
+
+Console.ReadKey();
+
