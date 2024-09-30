908b5a406943adb309e5e880a4256b885438a736
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-09-30 16:04:16 +0300

    null forgiving operator is implemented

---
+++ nullForgivingOperator/nullForgivingOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.ReadKey();
+
+void OddClassInit_ShallThrowExceptionWhenGivenNull()
+{
+    //validate this line throw an exception
+    new OddClass().Init(null!);
+}
+
+public class OddClass
+{
+    public string? Text { get; private set; }
+    private bool _isInitiated;
+
+    public void Init(string text)
+    {
+        Text = text ?? throw new NullReferenceException(nameof(text));
+        _isInitiated = true;
+    }
+
+    public void DoWork()
+    {
+        if (!_isInitiated)
+        {
+            throw new InvalidOperationException("The class sis not initialised");
+        }
+
+        Console.WriteLine("The length of text is: " + Text!.Length); // null forgiving operator
+    }
+}
