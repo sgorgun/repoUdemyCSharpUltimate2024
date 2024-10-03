8e86ed3c77f98ce26b2795c80819b051a3ef848b
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-03 23:30:50 +0300

    indexator exercise is implemented

---
+++ IndexatorExercise/IndexatorExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class PairOfArrays<TLeft, TRight>
+    {
+        private readonly TLeft[] _left;
+        private readonly TRight[] _right;
+
+        public PairOfArrays(
+            TLeft[] left, TRight[] right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public (TLeft Left, TRight Right) this[int indexLeft, int indexRight]
+        {
+            get
+            {
+                return (_left[indexLeft], _right[indexRight]);
+            }
+
+            set
+            {
+                _left[indexLeft] = value.Left;
+                _right[indexRight] = value.Right;
+            }
+        }
+    }
+}
