b5b7e5e854f3f3825ba8d0c96d037b21ebbc0c75
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-06 17:15:58 +0300

    init: base read method

---
+++ ReadonlyCollection/ReadonlyCollection/Program.cs
@@ -0,0 +1,16 @@
+﻿var planets = ReadPlanets();
+planets.Clear();
+
+Console.ReadKey();
+
+List<string> ReadPlanets()
+{
+    var result = new List<string>
+    {
+        "Alderaan",
+        "Coruskant",
+        "Bespin"
+    };
+
+    return result;
+}
