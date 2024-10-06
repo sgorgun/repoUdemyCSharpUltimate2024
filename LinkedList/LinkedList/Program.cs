5845565b98423efae22b9e6352227d74b60134b6
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 00:38:11 +0300

    Linked list is implemented

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿var linkedList = new LinkedList<int>();
+linkedList.AddFirst(1);
+linkedList.AddLast(2);
+linkedList.AddLast(3);
+linkedList.AddFirst(4);
+linkedList.Remove(2);
+
+foreach (var linkedItem in linkedList)
+{
+    Console.Write(linkedItem + " ");
+}
+
+Console.ReadKey();
