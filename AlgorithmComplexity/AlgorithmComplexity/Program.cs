0ad6689be241a2257d8877557c4a2f07c552fb4e
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-06 22:29:17 +0300

    Binary search alghoritm is added

--- AlgorithmComplexity/AlgorithmComplexity/Program.cs
+++ AlgorithmComplexity/AlgorithmComplexity/Program.cs
@@ -1,4 +1,4 @@
-﻿bool Contains<T> (T itemToCheck, IEnumerable<T> input) // O(N) linear complexity
+﻿bool Contains<T>(T itemToCheck, IEnumerable<T> input) // O(N) linear complexity
 {
     foreach (var item in input)
     {
@@ -11,7 +11,7 @@
     return false;
 }
 
-void Quadratic<T> (IEnumerable<T> input) // O(N*N) quadratic complexity
+void Quadratic<T>(IEnumerable<T> input) // O(N*N) quadratic complexity
 {
     foreach (var itemA in input)
     {
@@ -22,14 +22,40 @@ void Quadratic<T> (IEnumerable<T> input) // O(N*N) quadratic complexity
     }
 }
 
-T GetAtIndex<T> (List<T> input, int index) // O(1) constant complexity
+T GetAtIndex<T>(List<T> input, int index) // O(1) constant complexity
 {
     return input[index];
 }
 
 // O(log N) logarithmic complexity - binary search algorithm
 
-void SomeMethod<T> (IEnumerable<T> inputA, IEnumerable<T> inputB) // O(N*M) - two different collections
+int? BinarySearch<T>(int itemToCheck, List<int> input)
+{
+    int rightBound = 0;
+    int leftBound = input.Count() - 1;
+
+    while (leftBound <= rightBound)
+    {
+        int middle = (leftBound + rightBound) / 2;
+
+        if (input[middle] == itemToCheck)
+        {
+            return middle;
+        }
+        else if (input[middle] < itemToCheck)
+        {
+            rightBound = middle + 1;
+        }
+        else
+        {
+            leftBound = middle - 1;
+        }
+    }
+
+    return -1;
+}
+
+void SomeMethod<T>(IEnumerable<T> inputA, IEnumerable<T> inputB) // O(N*M) - two different collections
 {
     foreach (var itemA in inputA)
     {
