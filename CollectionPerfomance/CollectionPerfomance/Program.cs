d2f218c36e599a3c07ab87833d158dc2906cf889
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 00:10:46 +0300

    Collections perfomance test is implemented

---
+++ CollectionPerfomance/CollectionPerfomance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+var input = Enumerable.Range(0, 100_000_000).ToArray();
+
+Stopwatch sw = Stopwatch.StartNew();
+
+var list = new List<int>(input); // input as parameter of constructor - 200 ms
+//foreach (var item in input)// list iteration - 600 ms
+//{
+//    list.Add(item);
+//}
+
+sw.Stop();
+Console.WriteLine($"Took: {sw.ElapsedMilliseconds} ms");
+
+list.Clear(); // clean the list
+list.TrimExcess(); // trim the list if we don't need a big size anymore
+
+list.AddRange(input); //add elements into the end of list
+list.RemoveRange(5, 10); //for removing some elements
+list.RemoveAt(7); // remove element at special index
+list.Remove(99); // remove first found element 
+
+Console.ReadKey();
