eebff3cad92afb8bf1908573e5558ce504cf7170
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 22:27:09 +0300

    init: without yeld operator

---
+++ Yield/Yield/Program.cs
@@ -0,0 +1,28 @@
+﻿var smallSubset = GenerateEvenNumbers()
+    .Skip(5)
+    .Take(10);
+
+foreach (var number in GenerateEvenNumbers())
+{
+    if (number == 50)
+    {
+        break;
+    }
+    Console.WriteLine(number);
+}
+
+var firstEvenNumber = GenerateEvenNumbers().First();
+
+Console.ReadKey();
+
+IEnumerable<int> GenerateEvenNumbers()
+{
+    var result = new List<int>();
+
+    for (int i = 0; i < int.MaxValue; i += 2)
+    {
+        result.Add(i);
+    }
+
+    return result;
+}
