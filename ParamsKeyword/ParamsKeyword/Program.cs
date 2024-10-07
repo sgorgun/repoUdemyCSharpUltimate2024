e1a71245c21b30ff4d2c79e0421d5669c4594a75
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 17:57:03 +0300

    Params keyword is implemented.

---
+++ ParamsKeyword/ParamsKeyword/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(Calculator.Add(1, 2));
+Console.WriteLine(Calculator.Add(1, 2, 3));
+Console.WriteLine(Calculator.Add(1, 2, 3, 4));
+
+Console.ReadKey();
+
+public static class Calculator
+{
+    public static int Add(params int[] numbers) =>
+        numbers.Sum();
+}
