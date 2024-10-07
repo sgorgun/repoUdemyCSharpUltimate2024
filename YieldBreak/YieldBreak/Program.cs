10be98f53e48a4fff5dcab14d7518478409456d9
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-08 00:23:24 +0300

    Yield break is implemented

---
+++ YieldBreak/YieldBreak/Program.cs
@@ -0,0 +1,23 @@
+﻿var numbers = new int[] { 1, 4, 2, -5, 6, -2, 1 };
+foreach (var number in GetBeforeFirstNegative(numbers))
+{
+    Console.WriteLine(number);
+}
+
+Console.ReadKey();
+
+IEnumerable<int> GetBeforeFirstNegative(IEnumerable<int> input)
+{
+    foreach (int number in input)
+    {
+        if (number >= 0)
+        {
+            yield return number;
+        }
+
+        else
+        {
+            yield break; 
+        }
+    }
+}
