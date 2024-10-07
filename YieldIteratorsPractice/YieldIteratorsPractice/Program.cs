c197b7b927ec23a0f1b707a011adb36e65658649
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 23:50:42 +0300

    Yield mechanic for debugging

---
+++ YieldIteratorsPractice/YieldIteratorsPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿foreach (var number in GetSingleDigitNumbers())
+{
+    Console.WriteLine(number);
+}
+
+Console.ReadLine();
+
+IEnumerable<int> GetSingleDigitNumbers()
+{
+    yield return 0;
+    yield return 1;
+    yield return 2;
+    yield return 3;
+    yield return 4;
+    yield return 5;   
+    yield return 6;
+    yield return 7;
+    yield return 8;
+    yield return 9;
+}
