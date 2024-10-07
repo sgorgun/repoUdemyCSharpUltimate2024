fdb36276278dfbb43d144fafd61ff82c04a3c39d
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-08 00:09:23 +0300

    YieldHashset is implemented

---
+++ YieldHashset/YieldHashset/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = new[] { "a", "b", "a", "c", "d", "b" };
+foreach (var item in Distinct(input))
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
+
+IEnumerable<T> Distinct<T>(IEnumerable<T> input)
+{
+    var hashSet = new HashSet<T>();
+    foreach (var item in input)
+    {
+        if (!hashSet.Contains(item))
+        {
+            hashSet.Add(item);
+            yield return item;
+            Console.WriteLine("After yield.");
+        }
+    }
+}
