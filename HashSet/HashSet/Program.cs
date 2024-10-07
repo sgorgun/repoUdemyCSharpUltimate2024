11d611ea34c7726ae8d68dced0b218eedb0e0a43
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 16:55:39 +0300

    HashSet is implemented

---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,19 @@
+﻿var hashSet = new HashSet<string>();
+hashSet.Add("aaa");
+hashSet.Add("aaa");
+hashSet.Add("bbb");
+
+Console.ReadKey();
+
+public class SpellChecker
+{
+    private readonly HashSet<string> _correctWords = new()
+    {
+        "dog", "cat", "fish"
+    };
+
+    public bool IsCorrect(string word) =>
+        _correctWords.Contains(word);
+    public void AddCorrectWord(string word) =>
+        _correctWords.Add(word);
+}
