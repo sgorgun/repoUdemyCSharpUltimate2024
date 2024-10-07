29135586bbd2b05dcbf7af49cf531668c010aae3
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-07 17:27:16 +0300

    Queue is implemented

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,29 @@
+﻿var queue = new Queue<string>();
+queue.Enqueue("a");
+queue.Enqueue("b");
+queue.Enqueue("c");
+queue.Enqueue("d");
+
+var first = queue.Dequeue();
+var second = queue.Peek();
+
+Console.WriteLine(first);
+Console.WriteLine();
+Console.WriteLine(second);
+Console.WriteLine();
+
+foreach (var item in queue)
+{
+    Console.WriteLine(item);
+}
+
+var priorityQueue = new PriorityQueue<string,int>();
+priorityQueue.Enqueue("a", 5);
+priorityQueue.Enqueue("b", 5);
+priorityQueue.Enqueue("c", 2);
+priorityQueue.Enqueue("d", 3);
+
+var firstPriority = priorityQueue.Dequeue();
+
+
+Console.ReadKey();
