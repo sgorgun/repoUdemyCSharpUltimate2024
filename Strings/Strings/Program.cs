6198a1837e78e1a7912390b6334911004d644b39
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-10 17:39:07 +0300

    Strings is implemented

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+//Console.OutputEncoding = Encoding.Unicode;
+char greek = 'Ω';
+char arabic = 'ػ';
+char symbol = '!';
+Console.WriteLine(greek);
+Console.WriteLine(arabic);
+Console.WriteLine(Console.OutputEncoding);
+
+string text = "abc";
+text += "d";
+Console.WriteLine(text);
+
+var upperCase = text.ToUpper();
+Console.WriteLine(upperCase);
+
+string text1 = "abc";
+string text2 = "abc";
+Console.WriteLine(text1.Equals(text2));
+Console.WriteLine(text1 == text2);
+Modify(text1);
+Console.WriteLine(text1);
+
+Console.ReadKey();
+
+void Modify (string input)
+{
+    input += "xyz";
+}
