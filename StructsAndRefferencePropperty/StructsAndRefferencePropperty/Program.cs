5bf3e7802e799217092390a71e8b82f72c616c6f
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-16 16:23:07 +0300

    Struct refference problem is implemented

---
+++ StructsAndRefferencePropperty/StructsAndRefferencePropperty/Program.cs
@@ -0,0 +1,14 @@
+﻿var test = new TestStruct
+{
+    List = new List<int> { 1, 2, 3 }
+};
+
+var other = test; //it's copy link. It's unexpected behavior from structure. 
+other.List.Clear();
+
+Console.ReadKey();
+
+public struct TestStruct
+{
+    public List<int> List { get; init; }
+}
