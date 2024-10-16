c9d9fe77e42ea3192b9ebc1b52a67d57c5315802
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-16 16:56:35 +0300

    StringBuilder is implemented

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+
+const int Count = 200_000;
+var text = string.Empty;
+Stopwatch stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < Count; i++)
+{
+    text += "a";
+}
+stopwatch.Stop();
+Console.WriteLine($"Concatenation took {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Restart();
+var stringBuilder = new StringBuilder();
+for (int i = 0; i < Count; i++)
+{
+    stringBuilder.Append("a");
+}
+stopwatch.Stop();
+var text2 = stringBuilder.ToString();
+Console.WriteLine($"StringBuilder took {stopwatch.ElapsedMilliseconds} ms");
+
+Console.ReadKey();
