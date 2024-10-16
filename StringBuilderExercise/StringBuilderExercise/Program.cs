9aa953478aa3c6eaae1dd82e2b146fc0a17766e5
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-10-16 17:08:39 +0300

    StringBuilder exercise is implemented

---
+++ StringBuilderExercise/StringBuilderExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Coding.Exercise
+{
+    public static class StringBuilderExercise
+    {
+        public static string Reverse(string input)
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = input.Length - 1; i > -1; i--)
+            {
+                stringBuilder.Append(input[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ StringBuilderExercise/readme.md
@@ -0,0 +1,13 @@
+
+StringBuilder - The Reverse method
+Using the StringBuilder class, implement the Reverse method, which takes a string and returns it reversed.
+
+For example:
+
+for input "abc" result shall be "cba"
+
+for input "Hello" result shall be "olleH"
+
+for input "" result shall be ""
+
+for input "one two three" result shall be "eerht owt eno"
